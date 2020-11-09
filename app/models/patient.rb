d5ecffc98a1431b7536ad895547730fe158b37f5
Zach Stearns <zach.stearns92@gmail.com>
2020-11-09 11:47:28 -0700

    Replaced the oldest_to_youngest method so you could see what I was trying

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,8 @@ class Patient < ApplicationRecord
   validates_presence_of :name, :age
   has_many :doctor_patients
   has_many :doctors, through: :doctor_patients
+
+  def oldest_to_youngest
+    order(age: :desc).pluck(:name)
+  end
 end
