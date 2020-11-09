d9ff6544a29a7658f2eb7fb28cdd66d6dc7da3d0
Zach Stearns <zach.stearns92@gmail.com>
2020-11-09 11:02:30 -0700

    WIP extension

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,12 @@
 class Patient < ApplicationRecord
   validates_presence_of :name, :age
   belongs_to :doctor
+
+  def oldest_to_youngest
+    order(age: :desc).pluck(:name)
+  end
+
+  def o
+    order(:age)
+  end
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -9,4 +9,57 @@ describe Patient, type: :model do
   describe "relationships" do
     it { should belong_to :doctor }
   end
+
+  describe "model test" do
+    before(:each) do
+      @hospital = Hospital.create!({
+        name: 'Grey Sloan Memorial Hospital'
+        })
+
+      @doc_1 = Doctor.create!({
+        name: 'Meredith Grey',
+        specialty: 'General Surgery',
+        university: 'Harvard University',
+        hospital_id: @hospital.id
+        })
+
+      @doc_2 = Doctor.create!({
+        name: 'Alex Karev',
+        specialty: 'Pediatric Surgery',
+        university: 'Johns Hopkins University',
+        hospital_id: @hospital.id
+        })
+
+      @patient_1 = Patient.create!({
+        name: 'Katie Bryce',
+        age: 24,
+        doctor_id: @doc_1.id
+        })
+
+      @patient_2 = Patient.create!({
+        name: 'Denny Duquette',
+        age: 39,
+        doctor_id: @doc_1.id
+        })
+
+      @patient_3 = Patient.create!({
+        name: 'Rebecca Pope',
+        age: 32,
+        doctor_id: @doc_1.id
+        })
+
+      @patient_4 = Patient.create!({
+        name: 'Zola Shepherd',
+        age: 2,
+        doctor_id: @doc_2.id
+        })
+    end
+
+    it "#oldest_to_youngest" do
+      patients = Patient.all
+require "pry"; binding.pry
+      expected = ['Denny Duquette', 'Rebecca Pope', 'Katie Bryce', 'Zola Shepherd']
+      expect(patients.oldest_to_youngest).to eq(expected)
+    end
+  end
 end
